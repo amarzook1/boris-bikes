048f951e2c762299a7c68af5355d32f61e3512d9
Ahmed <ahmed.marzook@cgi.com>
2018-10-02 15:39:11 +0000

    Chapter 11 Half way through

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,11 @@ class DockingStation
     def release_bike
         Bike.new
     end
+    
     def dock(bike)
+        bike
+    end
+    
+    def bike
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,9 +6,20 @@ describe DockingStation do
   end
   
   it 'release working bikes' do
-      bike = subject.release_bike
-      expect(bike).to be_working
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
   
-  it { is_expected.to respond_to(:dock).with(1).argument }
+  it 'respond to bike' do
+    is_expected.to respond_to(:bike)
+  end
+  
+  it 'responds to docking bike' do
+    is_expected.to respond_to(:dock).with(1).argument
+  end
+  
+  it 'respond to docking a bike' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+  end
 end
