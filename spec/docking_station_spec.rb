9963cb565a6612faa07aea069132f43e712722dc
Ahmed <ahmed.marzook@cgi.com>
2018-10-03 15:10:17 +0000

    random commit

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,8 +6,8 @@ describe DockingStation do
   end
   
   it 'release working bikes' do
-    bike = subject.release_bike #release_bike is a method
-    expect(bike).to be_working #"be_" is boolean in this case be_(method) returns true for bike is working
+    bike = Bike.new
+    expect(bike).to be_working
   end
   
   it 'respond to bike' do
@@ -28,5 +28,12 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+  describe '#release_bike' do
+    it 'release a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
 end
