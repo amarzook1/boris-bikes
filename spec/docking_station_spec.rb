435aa0f2d2eeddfd0eacc6c21ce62aa4243cbd9f
Ahmed <ahmed.marzook@cgi.com>
2018-10-03 14:09:03 +0000

    Chapter 11 Docking Bike and Checking Station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,20 +6,27 @@ describe DockingStation do
   end
   
   it 'release working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    bike = subject.release_bike #release_bike is a method
+    expect(bike).to be_working #"be_" is boolean in this case be_(method) returns true for bike is working
   end
   
   it 'respond to bike' do
-    is_expected.to respond_to(:bike)
+    expect(subject).to respond_to(:bike)
   end
   
-  it 'responds to docking bike' do
-    is_expected.to respond_to(:dock).with(1).argument
+  it 'responds to docking' do
+    expect(subject).to respond_to(:dock).with(1).argument
+    #'it { is_expected.to respond_to(:dock).with(1).argument } <= One line test
   end
   
-  it 'respond to docking a bike' do
-      bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+  it 'docking station responds back with bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+  
+  it 'Checking if bike is in docking station' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
